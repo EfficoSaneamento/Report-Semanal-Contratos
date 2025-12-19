49e587751ad795785f7d33a527aaa674d3234f34
EfficoSaneamento <alvaro.santos@effico.com.br>
2025-12-19 19:53:07 +0000

    ajuste visual

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbybeMFf5PE9G2LrWshRpT_i0rvtEus7H6nrvR1KnjbJtzV9_Fn491ZjumwZz0XtYSyKgQ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzUSzjn5x5UcbMbelKPrRNWJcdbJWhzU3ZG16SdxxwmHeafvfNdxXC25VKHQ7EsJeco7w/exec';
 
 /* =========================
    TOKEN
